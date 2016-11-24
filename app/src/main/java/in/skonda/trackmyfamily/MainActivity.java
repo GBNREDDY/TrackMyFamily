4f7b650f973cd3e5d391f6119c8bc6618ca8e66e
trackfamily <myyoutestmail@gmail.com>
2016-11-24 20:33:46 +0530

    first commit message

--- app/src/main/java/in/skonda/trackmyfamily/MainActivity.java
+++ app/src/main/java/in/skonda/trackmyfamily/MainActivity.java
@@ -3,7 +3,6 @@ package in.skonda.trackmyfamily;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Typeface;
-import android.preference.PreferenceManager;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
