15bd34c08defb02ba98a45735d90422aa48eded2
trackfamily <myyoutestmail@gmail.com>
2016-12-22 11:57:14 +0530

    Location Retriving complete

--- app/src/main/java/in/skonda/trackmyfamily/Listf.java
+++ app/src/main/java/in/skonda/trackmyfamily/Listf.java
@@ -7,7 +7,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 
-public class Listf extends Fragment {
+public class Listf extends Fragment  {
 
     public Listf(){
 
@@ -16,7 +16,8 @@ public class Listf extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+
+
         return inflater.inflate(R.layout.fragment_listf, container, false);
     }
 
